131a6301d84a803fc8fcdefa5ce9e9ab7097f433
Martin Kelm <martinkelm@shrt.ws>
2014-10-16 16:12:14 +0200

    basic logic to handle display module

--- ncmct.c
+++ ncmct.c
@@ -2,5 +2,17 @@
 #include "display.h"
 
 int main(void) {
+
+  dsp_init();
+  dsp_windows_init();
+
+  dsp_set_meta("Mass Control Tycoon", "0.0.1 alpha", "Martin Kelm", "2014");
+
+  dsp_set_location("Earth");
+  dsp_set_output("Welcome to Mass Control Tycoon ...");
+
+  char *input = dsp_get_input();
+
+  dsp_end();
   return 0;
 }
