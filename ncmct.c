613055e32e06515866380bf860e064090f630d9f
Martin Kelm <martinkelm@shrt.ws>
2014-10-17 09:55:13 +0200

    added logic for main timer

--- ncmct.c
+++ ncmct.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
 #include "display.h"
 #include "round.h"
 #include "companies.h"
 
+void *main_timer(void *val);
+
 int main(void) {
+
+  pthread_t pt_main_timer;
+  pthread_create(&pt_main_timer, NULL, &main_timer, NULL);
 
   investment_companies_init();
 
@@ -15,10 +22,19 @@ int main(void) {
   dsp_set_location_cash("Anuka", 10000);
   dsp_set_output("Welcome to Mass Control Tycoon ...");
 
-  round_init();
-
   char *input = dsp_get_input();
 
   dsp_end();
   return 0;
+}
+
+void *main_timer(void *val) {
+  while (1) {
+    // do main timer logic
+    if (round_get_current() == 0 || round_get_current_end() == 1) {
+      round_init();
+    }
+    sleep(1);
+  }
+  pthread_exit(NULL);
 }
