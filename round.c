7e98d39a3b0f11883bf0734d4806c2a587524d06
Martin Kelm <martinkelm@shrt.ws>
2014-10-17 09:54:48 +0200

    changes for round end logic

--- round.c
+++ round.c
@@ -4,6 +4,7 @@
 #include "display.h"
 
 int current_round = 0;
+int current_round_end = 0;
 
 int max_round_seconds = 180;
 int current_round_seconds = 0;
@@ -17,6 +18,7 @@ void round_init(void) {
 
 void round_set_next(void) {
   current_round++;
+  current_round_end = 0;
   current_round_seconds = 0;
 }
 
@@ -24,17 +26,21 @@ int round_get_current(void) {
   return current_round;
 }
 
+int round_get_current_end(void) {
+  return current_round_end;
+}
+
 int round_get_remaining_seconds(void) {
   return max_round_seconds - current_round_seconds;
 }
 
 void *round_timer(void *val) {
-  while (1) {
+  do {
     dsp_set_round_info(round_get_current(), round_get_remaining_seconds());
     sleep(1);
     current_round_seconds++;
-    if (current_round_seconds > max_round_seconds)
-      round_set_next();
-  }
+  } while (current_round_seconds < max_round_seconds);
+  dsp_set_round_info(round_get_current(), round_get_remaining_seconds());
+  current_round_end = 1;
   pthread_exit(NULL);
 }
--- round.h
+++ round.h
@@ -1,5 +1,6 @@
 void round_init(void);
 void round_set_next(void);
 int round_get_current(void);
+int round_get_current_end(void);
 int round_get_remaining_seconds(void);
 void *round_timer(void *val);
